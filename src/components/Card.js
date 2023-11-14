1e14208af249bfdbf2eae8bd30700f4088b4ccd3
BenMeie <crazycowmm@gmail.com>
2023-11-14 11:42:41 -0800

    previews, neat effects

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,25 @@ export default function Card({item, id}) {
     } else {
         image = item.cover_medium;
     }
+
+    const play = (e) => {
+        e.currentTarget.parentElement.parentElement.parentElement.querySelectorAll(".audio").forEach((audio) => {
+            audio.pause()
+        })
+        e.currentTarget.parentElement.querySelector(`#play_${id}`).play();
+    }
+
     return (
         <div id={id} className="card">
-            <img src={image}></img>
-            <h4>{item.title}</h4>
+            <img className="background" src={image}></img>
+            <div className="content">
+                <img className="art" src={image}></img>
+                <h4>{item.title}</h4>
+                <button className="play_button" onClick={(e) => play(e)}>Play</button>
+                <audio id={`play_${id}`} className="audio">
+                    <source src={item.preview} type="audio/mpeg"/>    
+                </audio>
+            </div>
         </div>
     )
 }
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,18 @@
 import { Filter } from "react-feather"
+import ContentGrid from "../components/ContentGrid"
+import { useEffect, useState } from "react"
+import { getTopSongs } from "../data/getSongs"
 
 export default function SearchPage() {
+    const [content, setContent] = useState([])
+
+    useEffect(() => {
+        const getContent = async () => {
+            setContent(await getTopSongs());
+        }
+        getContent();
+    }, [content])
+
     return (
         <>
             <div className="search_and_filter">
@@ -9,6 +21,7 @@ export default function SearchPage() {
                     <button className="secondary_button"><Filter /></button>
                 </div>
             </div>
+            <ContentGrid content={content} />
         </>
     )
 }
